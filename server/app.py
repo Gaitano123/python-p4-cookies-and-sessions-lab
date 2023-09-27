dcfa89b105db1cd29af4f33796113018d7902e1f
Gaitano <gaitano.george@student.moringaschool.com>
2023-09-27 18:03:38 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -22,13 +22,44 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    list = []
+    for article in articles:
+        atricle_dict = {
+            "id": article.id,
+            "author": article.author,
+            "title": article.title,
+            "content": article.content,
+            "preview": article.preview,
+            "minutes_to_read": article.minutes_to_read,
+            "date": article.date,
+            "user_id": article.user_id  
+        }
+        list.append(atricle_dict)
+    return make_response(jsonify(list), 200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    session ['page_views'] = session.get('page_views', 0)
+    
+    session['page_views'] += 1
+     
+    if session['page_views'] > 3:
+        return make_response(jsonify({'message' : 'Maximum pageview limit reached'}), 401)
+    
+    article = Article.query.filter(Article.id == id).first()
+    article_dict ={
+            "id": article.id,
+            "author": article.author,
+            "title": article.title,
+            "content": article.content,
+            "preview": article.preview,
+            "minutes_to_read": article.minutes_to_read,
+            "date": article.date,
+            "user_id": article.user_id 
+    }
+    
+    return make_response(jsonify(article_dict), 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
